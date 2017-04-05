3a4fa23cec94de31057233122a4ad2482b7ae8e9
Linda Castillor <linda@raidho.mx>
2017-04-05 16:15:45 -0500

    :sparkles: Canvas Grid #1 Update

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,25 @@ if(windowWidth > 768){
 		theme:"dark"
 	});
 }
+
+// CIRCLE PROGRESS BAR: DRAW GREEN LINE
+$(".circle-progress-bar").each(function(){
+	var val = parseInt($(this).find('#number').text());
+	var $circle = $(this).find('#svg #bar');
+	if (isNaN(val)) {
+		val = 100;
+	}
+	else{
+		var r = $circle.attr('r');
+		var c = Math.PI*(r*2);
+
+		if (val < 0) { val = 0;}
+		if (val > 100) { val = 100;}
+
+		var pct = ((100-val)/100)*c;
+
+		$circle.css({ strokeDashoffset: pct});
+
+		$('#cont').attr('data-pct',val);
+	}
+});
